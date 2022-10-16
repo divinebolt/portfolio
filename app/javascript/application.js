9b2e9127d5f7303915468cb2a93dcaa0ce013055
JoÃao Bernardo <joaogoncalob@gmail.com>
2022-10-16 20:26:50 +0100

    javacript is now working

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import "bootstrap"
+// import "@hotwired/turbo-rails"
+// import "./controllers"
+// import "bootstrap"
 
 // inside app/javascript/application.js
 // Entry point for the build script in your package.json
